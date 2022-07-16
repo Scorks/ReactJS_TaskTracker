efcb109a3b4b918cf8d2f4c4efeb9c8539894692
Caro Strickland <carostrickland321@gmail.com>
2022-07-16 05:04:09 -0400

    production build

---
+++ src/components/Task.js
@@ -0,0 +1,12 @@
+import { FaAsterisk } from 'react-icons/fa'
+
+const Task = ({task, onDelete, onToggle}) => {
+  return (
+    <div className={`task ${task.reminder ? 'reminder' : ''}`} onDoubleClick={() => onToggle(task.id)}>
+      <h3>{task.text} <FaAsterisk style={{color: 'red', cursor: 'pointer'}} onClick={() => onDelete(task.id)}/></h3>
+      <p> {task.day} </p>
+    </div>
+  )
+}
+
+export default Task
